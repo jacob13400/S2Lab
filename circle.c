924fb0188b06127894796944788eeea20fdcea69
Zeref Dragneel <45292374+jacob13400@users.noreply.github.com>
2019-05-18 10:00:14 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -7,7 +7,7 @@ int main() {
 
     double r;int n;
     double x,y;int i;
-    double b[2];int c[1000000];
+    double b[2];
     scanf("%lf",&r);
     scanf("%d",&n);int p=0;
     scanf(" %lf %lf",&x,&y);
@@ -16,14 +16,15 @@ int main() {
         scanf(" %lf",&b[1]);
         double z=sqrt(pow(b[0]-x,2)+pow(b[1]-y,2)*1.0);
         if(z<=abs(r)){
-            c[i]=1;p=1;}
-        else
-            c[i]=0;
-    }
-    for(i=0;i<n;i++){
-        if(c[i]==1)
             printf("%d ",i);
+            p=1;}
+
+
     }
+
+
+
+
     if(p==0)
         printf("-1");
     return 0;
